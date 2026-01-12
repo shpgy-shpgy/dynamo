31b9990ae5d07ba3804aadf6fdec0323f400b313
shpgy-shpgy <875664365@qq.com>
2026-01-12 13:36:05 +0800

    support onlly-text
    Signed-off-by: shpgy-shpgy <875664365@qq.com>

--- components/src/dynamo/trtllm/request_handlers/handler_base.py
+++ components/src/dynamo/trtllm/request_handlers/handler_base.py
@@ -180,7 +180,7 @@ class HandlerBase:
             embeddings: Optional tensor or dict containing embeddings for multimodal processing
         """
         logging.debug(f"Request: {request}")
-        t0 = time.perf_counter()
+        # t0 = time.perf_counter()
 
         # Default to text-based input. This will be overwritten if multimodal
         # content is found and processed.
@@ -196,8 +196,8 @@ class HandlerBase:
             # text-only flow
             processed_input = request.get("token_ids")
         
-        t1 = time.perf_counter()
-        print(f"************Input processing time: {(t1 - t0)*1000:.4f} ms")
+        # t1 = time.perf_counter()
+        # print(f"************Input processing time: {(t1 - t0)*1000:.4f} ms")
 
         # Check if there is an error in the publisher error queue
         publishers_error = (
@@ -264,8 +264,8 @@ class HandlerBase:
             sampling_params.logits_processor = adapters
 
         
-        t2 = time.perf_counter()
-        print(f"************Sampling params setup time: {(t2 - t1)*1000:.4f} ms")
+        # t2 = time.perf_counter()
+        # print(f"************Sampling params setup time: {(t2 - t1)*1000:.4f} ms")
         
         try:
             # NEW: Updated engine call to include multimodal data
@@ -328,8 +328,8 @@ class HandlerBase:
                     yield out
                     num_output_tokens_so_far = next_total_toks
             
-            t3 = time.perf_counter()
-            print(f"************Generation time: {(t3 - t2)*1000:.4f} ms")
+            # t3 = time.perf_counter()
+            # print(f"************Generation time: {(t3 - t2)*1000:.4f} ms")
 
         # 1. Client cancellation - don't shutdown
         except asyncio.CancelledError:
